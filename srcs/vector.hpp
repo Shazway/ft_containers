ae05f14a0b3446a956cbb36d3c6871401a4055f0
Telli MORAGLIS <tmoragli@e1r5p11.clusters.42paris.fr>
2022-11-22 19:57:04 +0100

    Added copy of vector class, constructors, accessors, destructor, comparison operators, assignation operators, utilitary functions

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -6,21 +6,22 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 00:07:02 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/11/21 00:44:39 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/11/22 19:55:51 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_HPP
-#define VECTOR_HPP
+# ifndef VECTOR_HPP
+# define VECTOR_HPP
 
-
+# include <memory>
+# include <exception>
 namespace ft
 {
-	template < typename T, typename Allocate = std::allocator<T> >
+	template < typename T, typename Alloc = std::allocator<T> >
 	class vector;
 
 	template <typename T>
-	void	swap(vector<T>, &a, vector<T> &b){
+	void	swap(vector<T> &a, vector<T> &b){
 		a.swap(b);
 	}
 	//Faire les iterators
@@ -53,7 +54,7 @@ namespace ft
 	template <typename T>
 	bool	operator<(vector<T> const& v1, vector<T> const& v2)
 	{
-		if (v1 > v2)
+		if (v1 > v2 && v1 != v2)
 			return (false);
 		return (true);
 	}
@@ -65,7 +66,7 @@ namespace ft
 			return (true);
 		return (false);
 	}
-
+	template <typename T>
 	bool	operator>=(vector<T> const& v1, vector<T> const& v2)
 	{
 		if (v1 > v2 || v1 == v2)
@@ -82,9 +83,141 @@ template <typename T, typename Alloc>
 class ft::vector
 {
 public:
-	typedef T value_type;
-	typedef Alloc allocator_type;
-}
+	typedef T		value_type;
+	typedef Alloc	allocator_type;
+
+	typedef typename allocator_type::reference			reference;
+	typedef typename allocator_type::const_reference	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
+	//Iterators (les faire soit même)
+	typedef typename std::vector::iterator						iterator;
+	typedef typename std::vector::const_iterator				const_iterator;
+	typedef typename std::vector::reverse_iterator				reverse_iterator;
+	typedef typename std::vector::const_reverse_iterator		const_reverse_iterator;
+
+	typedef typename allocator_type::size_type			size_type;
+	typedef typename allocator_type::difference_type	difference_type;
+private:
+	pointer			_data;
+	size_type		_capacity;
+	size_type		_size;
+	allocator_type	_allocator;
+public:
+	//CONSTRUCT/DESTRUCT
+	explicit vector(const allocator_type &alloc = allocator_type()): _data(NULL), _capacity(0), _size(0), allocator(alloc){}
+	explicit vector(size_type count, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()): _data(NULL), _capacity(0), _size(0){
+		assign(count, val); // a coder
+	}
+	//template <class InputIterator>
+	/*vector(InputIterator first, InputIterator, last, allocator_type const& alloc = allocator_type()): _data(NULL), _capacity(0), _size(0), _allocator(alloc)
+	{
+		//is integral
+		//init vector
+	}*/
+	vector(const vector &copy, allocator_type const& alloc = allocator_type() = allocator_type()): _data(NULL), _capacity(0), _size(0), allocator(alloc)
+	{
+		*this = copy;
+	}
+	//operator=
+	vector	&operator=(vector<value_type>)
+	~vector(){
+		clear();
+		_allocator.deallocate(_data, _capacity);
+	}
+	size_type	size() const
+	{
+		return (_size);
+	}
+	size_type	max_size() const
+	{
+		return (_allocator.max_size());
+	}
+	//resize
+	size_type	capacity() const
+	{
+		return (_capacity);
+	}
+	bool	empty() const
+	{
+		return (!_size);
+	}
+	//reserve
+	//ACCESS
+	reference		operator[](size_type index){
+		return (_data[index]);
+	}
+	const_reference	operator[](size_type index) const{
+		return (_data[index]);
+	}
+	reference		at(size_type index)
+	{
+		check_index(index); // a faire
+		return (_data[n]);
+	}
+	const_reference	at(size_type index) const
+	{
+		check_index(index); // a faire
+		return (_data[n]);
+	}
+	reference		front()
+	{
+		return (_data[0])
+	}
+	const_reference	front() const
+	{
+		return (_data[0])
+	}
+	reference		back()
+	{
+		return (_data[_size - 1]);
+	}
+	const_reference	back() const
+	{
+		return (_data[_size - 1]);
+	}
+	//assign inputiterator first, iterator last
+	//assign size n, value val
+	//push_back(const value_type &val)
+	void	pop_back()
+	{
+		_data[--_size].~value_type();
+	}
+	//insert pos val
+	//insert pos size val
+	//insert pos iterator first iterator last
+	//erase pos
+	//erase from first to last
+	void	swap(vector& elem)
+	{
+		vector	tmp = *this;
+	
+		*this = elem;
+		elem = tmp;
+	}
+	void	clear()
+	{
+		if (!_data)
+			return ;
+		for (size_type i = 0; i < _size; i++)
+			_allocator.destroy(_data + i);
+		_size = 0;
+	}
+	allocator_type	get_allocator() const
+	{
+		return (_allocator);
+	}
+private:
+	void	check_index(size_type	index) const
+	{
+		if (index < _size)
+			return ;
+		std::ostringstream	oss;
+	
+		oss << "vector::check_index index (which is " << index << ") >= this->size() (which is " << _size << ")";
+		throw (std::out_of_range(oss.str()));
+	}
+};
 
 
 #endif
