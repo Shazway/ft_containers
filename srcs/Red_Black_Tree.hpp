e64c1c23ac53d8b83abbfa658208426d475bd495
Telli MORAGLIS <tmoragli@e1r5p11.clusters.42paris.fr>
2022-12-20 21:35:30 +0100

    Added RBTree iterators, and const iterators

--- srcs/Red_Black_Tree.hpp
+++ srcs/Red_Black_Tree.hpp
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 18:14:38 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/12/20 18:06:57 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/12/20 21:34:34 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -156,6 +156,206 @@ namespace ft
 			}
 			return (parent);
 		}
+	};
+
+	template <typename T>
+	class RBTreeIterator
+	{
+		public:
+			typedef std::bidirectional_iterator_tag	iterator_category;
+
+			typedef T 								value_type;
+			typedef value_type&						reference;
+			typedef value_type*						pointer;
+			typedef	std::ptrdiff_t					difference_type;
+
+		private:
+			typedef RBTReeNode<T>					Node;
+
+			Node	*_current;
+			Node	*_start;
+			Node	*_end;
+		public:
+			RBTreeIterator(Node *n = NULL, Node *start = NULL, Node *end = NULL): _current(n), _start(start), _end(end)
+			{
+			}
+
+			RBTreeIterator(RBTreeIterator const& copy)
+			{
+				*this = copy;
+			}
+
+			RBTreeIterator	&operator=(RBTreeConstIterator const& assign)
+			{
+				_current = assign._current;
+				_start = assign._start;
+				_end = assign._end;
+				return (*this);
+			}
+
+			~RBTreeIterator()
+			{
+			}
+
+			bool	operator==(RBTreeIterator const& comp) const
+			{
+				return (_current == comp._current);
+			}
+
+			bool	operator!=(RBTreeIterator const& comp) const
+			{
+				return (!(*this == comp));
+			}
+
+			reference	operator*() const
+			{
+				return (_current->data);
+			}
+
+			RBTreeIterator	&operator++()
+			{
+				_current = Node::successor(current, start);
+				return (*this);
+			}
+
+			RBTreeIterator	operator++(int)
+			{
+				RBTreeIterator	it(*this);
+
+				_current = Node::successor(_current, _start);
+				return (it);
+			}
+
+			RBTreeIterator	&operator--()
+			{
+				_current = Node::predecessor(current, end);
+				return (*this);
+			}
+
+			RBTreeIterator	operator--(int)
+			{
+				RBTreeIterator	it(*this);
+
+				_current = Node::predecessor(_current, end);
+				return (it);
+			}
+
+			Node	*getCurrent() const
+			{
+				return (_current);
+			}
+
+			Node	*getStart() const
+			{
+				return (_start);
+			}
+
+			Node	*getEnd() const
+			{
+				return (_end);
+			}
+	};
+
+	template<typename T>
+	class RBTreeConstIterator
+	{
+		public:
+			typedef std::bidirectional_iterator_tag		iterator_category;
+			typedef T									value_type;
+			typedef value_type	const&					reference;
+			typedef	value_type	const					*pointer;
+			typedef	std::ptrdiff_t						difference_type;
+		
+		private:
+			typedef RBTreeNode<T>	Node;
+
+			Node	*_current;
+			Node	*_start;
+			Node	*_end;
+		public:
+			RBTreeConstIterator(Node *n = NULL, Node **start = NULL, Node *end = NULL): _current(n), _start(start), _end(end)
+			{
+			}
+		
+			RBTreeConstIterator(RBTreeConstIterator const& copy)
+			{
+				*this = copy;
+			}
+
+			RBTreeConstIterator(RBTreeIterator<value_type> const& copy)
+			{
+				*this = copy;
+			}
+
+			RBTreeConstIterator &operator=(RBTreeConstIterator const& assign)
+			{
+				_current = assign._current;
+				_start = assign._start;
+				_end = assign._end;
+
+				return (*this);
+			}
+
+			RBTreeConstIterator	&operator=(RBTreeIterator<value_type> const& assign)
+			{
+				_current = assign._current;
+				_start = assign._start;
+				_end = assign._end;
+
+				return (*this);
+			}
+
+			operator RBTReeIterator<value_type>() const
+			{
+				return (RBTreeIterator<value_type>(_current, _start, _end));
+			}
+
+			~RBTreeConstIterator()
+			{
+			}
+
+			bool	operator==(RBTreeConstIterator const& comp) const
+			{
+				return (_current == comp._current);
+			}
+
+			bool	operator!=(RBTreeConstIterator const& comp) const
+			{
+				return (!(*this == comp));
+			}
+
+			reference	operator*() const
+			{
+				return (_current->data_addr());
+			}
+
+			RBTreeConstIterator &operator++()
+			{
+				_current = Node::successor(_current, _start);
+				return (*this);
+			}
+
+			RBTreeConstIterator	operator++(int)
+			{
+				RBTreeConstIterator	it(*this);
+
+				_current = Node::successor(_current, _start);
+				return (it);
+			}
+
+			RBTreeConstIterator &operator--()
+			{
+				_current = Node::predecessor(_current, _end);
+				return (*this);
+			}
+
+			RBTreeConstIterator	operator--(int)
+			{
+				RBTreeConstIterator	it(*this);
+
+				_current = Node::predecessor(_current, _end);
+				return (it);
+			}
 	};
 }
 
